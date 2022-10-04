b1a94b3832f0eb895dc59de0676980673bffa7f5
mino87 <113091109+mino87@users.noreply.github.com>
2022-10-04 18:01:43 +0900

    06

--- main.c
+++ main.c
@@ -4,25 +4,33 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	printf("enter the calculation : ");
-	int x;
-	int y;
-	char c;
-	scanf("%d%c%d",&x,&c,&y);
-	int r;
+	int a=59;
+	int i;
+	int trial=1;
 	
-	if (c=='+')
-	   r=x+y;
-	else if (c=='-')
-	   r=x-y;
-	else if (c=='*')
-	   r=x*y;
-	else
-	   r=x/y;
-	
+	do
+	{
+		printf("input a number : ");
+		scanf("%d",&i);
+		if(i>a)
+		{
+		    printf("high!\n");
+		    trial=(trial+1);
+		}
 
-	printf("=%d\n",r);
+		else if(i<a)
+		{
+			printf("low!\n");
+		    trial=(trial+1);
+		}    
 	
+	}
+	while (i!=a);
+	printf("answer!\n");
+	printf("trial is %d\n",trial);    
+	
+	
+	 
 	
 	    	
 	return 0;

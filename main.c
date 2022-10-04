1cc98f3831f5346fb6bb1bf4da0e0477eb53877d
mino87 <113091109+mino87@users.noreply.github.com>
2022-10-04 16:40:45 +0900

    02

--- main.c
+++ main.c
@@ -4,18 +4,14 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	printf("정수를 하나 입력하시오. : ");
-   int i;
-   scanf("%d",&i);
-   
-   if (i>0)
-       printf("양수입니다..\n");
-    else if (i<0)
-       printf("음수입니다..\n");
-   else
-       printf("0입니다..\n");
+	int i;
+	printf("정수를 하나 입력하시오, : \n");
+	scanf ("%d", &i);
 	
-	    
-		
+	if(i>=0)
+	   printf("절대값은 %d 입니다.\n",i);
+	else
+	   printf("절대값은 %d 입니다.\n",-i);
+	    	
 	return 0;
 }
